9db10ac1535e9d888e333c4fdf729b1850aaf489
Vivek Raj <vivek.raj@druva.com>
2024-08-28 13:50:06 +0530

    checking on mac

--- 00greet/greetings.go
+++ 00greet/greetings.go
@@ -5,7 +5,7 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	println("hre in greetings functions")
+	println("hre in greetings functions cheking")
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
