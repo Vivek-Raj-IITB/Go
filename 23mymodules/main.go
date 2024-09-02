7d90121196186c8761ed15a606cff1cc82183e00
Vivek Raj <vivek.raj@druva.com>
2024-09-02 16:28:09 +0530

    modules in golang

---
+++ 23mymodules/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Hello mod in golang")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serverHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+
+}
+
+func greeter() {
+	fmt.Println("hey there mod users")
+}
+
+func serverHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>welcome to golang series</h1>"))
+}
